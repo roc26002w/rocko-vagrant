40f810f6bc2309e1072c486b60a6413f3e3e5c6d
Rocko <roc26002w@gmail.com>
2020-06-17 17:27:19 +0800

    [udpate] docker-compose install v1.26.0

--- multipass/provision/vendor/docker-compose.sh
+++ multipass/provision/vendor/docker-compose.sh
@@ -3,9 +3,12 @@
 echo '======= Install docker-compose.sh ======'
 
 
-apt install -y docker-compose
+# apt install -y docker-compose
 
 #Install docker-compose official git repository
-# curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
-# chmod +x /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
