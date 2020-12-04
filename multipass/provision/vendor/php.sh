d108bf6a169954015fbe0a19a7208c0c9c59c605
Rocko <roc26002w@gmail.com>
2020-12-04 10:21:55 +0800

    [add] php8.0 install

--- multipass/provision/vendor/php.sh
+++ multipass/provision/vendor/php.sh
@@ -19,6 +19,13 @@ apt-get update
 apt-get install -y --allow-change-held-packages \
 php-imagick php-memcached php-redis php-xdebug php-dev
 
+# PHP 8.0
+apt-get install -y --allow-change-held-packages \
+php8.0 php8.0-bcmath php8.0-bz2 php8.0-cgi php8.0-cli php8.0-common php8.0-curl php8.0-dba php8.0-dev \
+php8.0-enchant php8.0-fpm php8.0-gd php8.0-gmp php8.0-imap php8.0-interbase php8.0-intl php8.0-ldap \
+php8.0-mbstring php8.0-mysql php8.0-odbc php8.0-opcache php8.0-pgsql php8.0-phpdbg php8.0-pspell php8.0-readline \
+php8.0-snmp php8.0-soap php8.0-sqlite3 php8.0-sybase php8.0-tidy php8.0-xml php8.0-xsl php8.0-zip
+
 # PHP 7.4
 apt-get install -y --allow-change-held-packages \
 php7.4 php7.4-bcmath php7.4-bz2 php7.4-cgi php7.4-cli php7.4-common php7.4-curl php7.4-dba php7.4-dev \
