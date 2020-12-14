84e7c9f61ae0c82addb07b6c68ecfca10f7f2730
Rocko <roc26002w@gmail.com>
2020-12-14 17:24:21 +0800

    [update] pythone need finish install rocko-vim

--- multipass/install.sh
+++ multipass/install.sh
@@ -9,8 +9,8 @@ bash ./provision/vendor/node.sh
 bash ./provision/vendor/fzf.sh
 bash ./provision/vendor/php.sh
 bash ./provision/vendor/neovim.sh
-bash ./provision/vendor/python.sh
 bash ./provision/vendor/rocko-vim.sh
+bash ./provision/vendor/python.sh
 bash ./provision/vendor/laradock.sh
 bash ./provision/vendor/tmux.sh
 bash ./provision/vendor/ansible.sh
