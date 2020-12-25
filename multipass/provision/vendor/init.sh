7e25bdfb414a69b95c8f3a324746899d25d52c06
Rocko <roc26002w@gmail.com>
2020-12-25 17:33:10 +0800

    [update] git user mail params

--- multipass/provision/vendor/init.sh
+++ multipass/provision/vendor/init.sh
@@ -20,5 +20,5 @@ su - ${USER_NAME} -c "mkdir ${HOME_PATH}/.features"
 # auto gen ssh key
 su - ${USER_NAME} -c "ssh-keygen -f ${HOME_PATH}/.ssh/id_rsa -t rsa -N ''"
 
-su - ${USER_NAME} -c "git config --global user.email \"${GIT_USER_EMAIL}\""
+su - ${USER_NAME} -c "git config --global user.email \"${GIT_USER_MAIL}\""
 su - ${USER_NAME} -c "git config --global user.name \"${GIT_USER_NAME}\""
