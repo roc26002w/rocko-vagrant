32ae83c70284e10ca06926d1292f5059fde1f005
Rocko <roc26002w@gmail.com>
2020-12-25 17:45:49 +0800

    [tmp] update ohmyzsh script

--- multipass/provision/vendor/ohmyzsh.sh
+++ multipass/provision/vendor/ohmyzsh.sh
@@ -1,30 +1,31 @@
 #!/usr/bin/env bash
 echo '======= Install ohmyzsh.sh ======'
 
-if [ -f /home/ubuntu/.features/oh-my-zsh ]
+if [ -f ${HOME_PATH}/.features/oh-my-zsh ]
 then
     echo "oh-my-zsh already installed."
     exit 0
 fi
 
 touch ${HOME_PATH}/.features/oh-my-zsh
-chown -Rf ubuntu:ubuntu /home/ubuntu/.features
+
+export ZSH_CUSTOM=${HOME_PATH}/.oh-my-zsh/custom
+echo "======= ${ZSH_CUSTOM} ==========="
 
 # Install oh-my-zsh
-git clone https://github.com/ohmyzsh/ohmyzsh.git /home/ubuntu/.oh-my-zsh
-cp /home/ubuntu/.oh-my-zsh/templates/zshrc.zsh-template /home/ubuntu/.zshrc
-chown ubuntu:ubuntu /home/ubuntu/.zshrc
+git clone https://github.com/ohmyzsh/ohmyzsh.git ${HOME_PATH}/.oh-my-zsh
+cp ${HOME_PATH}/.oh-my-zsh/templates/zshrc.zsh-template ${HOME_PATH}/.zshrc
+chown ubuntu:ubuntu ${HOME_PATH}/.zshrc
 
 # zsh-lazyload
-git clone https://github.com/qoomon/zsh-lazyload /home/ubuntu/.oh-my-zsh/custom/plugins/zsh-lazyload
+git clone https://github.com/qoomon/zsh-lazyload ${ZSH_CUSTOM}/plugins/zsh-lazyload
 
 # zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/ubuntu/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
 
 # powerlevel10k
-git clone https://github.com/romkatv/powerlevel10k.git /home/ubuntu/.oh-my-zsh/custom/themes/powerlevel10k
+git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM}/themes/powerlevel10k
 
-chsh -s /bin/zsh ubuntu
-chown -R ubuntu:ubuntu /home/ubuntu/.oh-my-zsh
-
-echo "======= ${ZSH_CUSTOM} ==========="
+chsh -s /bin/zsh ${USER_NAME}
+chown -R ${USER_NAME}:${USER_NAME} ${HOME_PATH}/.oh-my-zsh
+chown -Rf ${USER_NAME}:${USER_NAME} ${HOME_PATH}/.features
