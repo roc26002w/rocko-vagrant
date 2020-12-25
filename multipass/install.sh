fa7ed54cb154d97aba54ce802249fbd6076d2d36
Rocko <roc26002w@gmail.com>
2020-12-25 16:40:04 +0800

    [update] refactor install script add user name input

--- multipass/install.sh
+++ multipass/install.sh
@@ -1,5 +1,34 @@
 #!/usr/bin/env bash
 
+function checkUserName(){
+  echo "=========將設定安裝使用者為 ${USERNAME} 請確認是否安裝? y/n"
+  read ans
+  answer=${ans,,}
+  if [ ${answer:0:1} != 'y' ]
+  then
+    exit 0
+  fi
+}
+
+# input has variable
+if [ ! -z $1 ]
+then
+  export USERNAME=$1
+fi
+
+# has USERNAME and not input variable
+if [[ ! -z ${USERNAME} && -z $1 ]]
+then
+  checkUserName
+fi
+
+# USERNAME not set
+if [ -z ${USERNAME} ]
+then
+  echo "沒有設定 USERNAME "
+  exit 0
+fi
+
 bash ./provision/vendor/init.sh
 bash ./provision/vendor/ohmyzsh.sh
 bash ./provision/vendor/docker.sh
