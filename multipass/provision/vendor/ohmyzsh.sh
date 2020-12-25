a3f4ed641df288bfcb594d0066e5b2e806577243
Rocko <roc26002w@gmail.com>
2020-12-25 16:30:47 +0800

    [update] zsh plugin

--- multipass/provision/vendor/ohmyzsh.sh
+++ multipass/provision/vendor/ohmyzsh.sh
@@ -14,8 +14,14 @@ chown -Rf ubuntu:ubuntu /home/ubuntu/.features
 git clone https://github.com/ohmyzsh/ohmyzsh.git /home/ubuntu/.oh-my-zsh
 cp /home/ubuntu/.oh-my-zsh/templates/zshrc.zsh-template /home/ubuntu/.zshrc
 chown ubuntu:ubuntu /home/ubuntu/.zshrc
+
+# zsh-lazyload
 git clone https://github.com/qoomon/zsh-lazyload /home/ubuntu/.oh-my-zsh/custom/plugins/zsh-lazyload
+
+# zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/ubuntu/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+# powerlevel10k
 git clone https://github.com/romkatv/powerlevel10k.git /home/ubuntu/.oh-my-zsh/custom/themes/powerlevel10k
 
 chsh -s /bin/zsh ubuntu
