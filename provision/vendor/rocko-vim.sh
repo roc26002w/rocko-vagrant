3702bda2c8ab12416f2325129f4e82fab93020b5
Rocko <roc26002w@gmail.com>
2024-03-27 14:04:35 +0800

    [update] 調整 ln config 設定

--- provision/vendor/rocko-vim.sh
+++ provision/vendor/rocko-vim.sh
@@ -14,7 +14,7 @@ chown -Rf ${USER_NAME}:${USER_NAME} ${HOME_PATH}/.features
 su ${USER_NAME} -c "git clone https://github.com/roc26002w/rocko-vim.git ${HOME_PATH}/.config/rocko-vim"
 su ${USER_NAME} -c "cd ${HOME_PATH}/.config/rocko-vim"
 
-su ${USER_NAME} -c "mkdir ${HOME_PATH}/.config/git"
+su ${USER_NAME} -c "mkdir -p ${HOME_PATH}/.config/git"
 
 su ${USER_NAME} -c "ln -sf ${HOME_PATH}/.config/rocko-vim/oh-my-zsh/.zshrc ${HOME_PATH}/.zshrc"
 su ${USER_NAME} -c "ln -sf ${HOME_PATH}/.config/rocko-vim/neovim/.config/nvim ${HOME_PATH}/.config/nvim"
