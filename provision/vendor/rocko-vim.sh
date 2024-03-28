fb814ee6c352400c5c53471f4e8c911f1bbed1f0
Rocko <roc26002w@gmail.com>
2024-03-28 09:08:39 +0800

    [update] git path ln command

--- provision/vendor/rocko-vim.sh
+++ provision/vendor/rocko-vim.sh
@@ -14,11 +14,11 @@ chown -Rf ${USER_NAME}:${USER_NAME} ${HOME_PATH}/.features
 su ${USER_NAME} -c "git clone https://github.com/roc26002w/rocko-vim.git ${HOME_PATH}/.config/rocko-vim"
 su ${USER_NAME} -c "cd ${HOME_PATH}/.config/rocko-vim"
 
-su ${USER_NAME} -c "mkdir -p ${HOME_PATH}/.config/git"
+su ${USER_NAME} -c "mkdir -p ${HOME_PATH}/.config"
 
 su ${USER_NAME} -c "ln -sf ${HOME_PATH}/.config/rocko-vim/oh-my-zsh/.zshrc ${HOME_PATH}/.zshrc"
 su ${USER_NAME} -c "ln -sf ${HOME_PATH}/.config/rocko-vim/neovim/.config/nvim ${HOME_PATH}/.config/nvim"
 su ${USER_NAME} -c "ln -sf ${HOME_PATH}/.config/rocko-vim/oh-my-zsh/.theme ${HOME_PATH}/.oh-my-zsh/custom/themes/.theme"
 su ${USER_NAME} -c "ln -sf ${HOME_PATH}/.config/rocko-vim/tig/.tigrc ${HOME_PATH}/.tigrc"
 su ${USER_NAME} -c "ln -sf ${HOME_PATH}/.config/rocko-vim/git/.gitconfig ${HOME_PATH}/.gitconfig"
-su ${USER_NAME} -c "ln -sf ${HOME_PATH}/.config/rocko-vim/git/.gitmessage.txt ${HOME_PATH}/.config/git/.gitmessage.txt"
+su ${USER_NAME} -c "ln -sf ${HOME_PATH}/.config/rocko-vim/git ${HOME_PATH}/.config/git"
